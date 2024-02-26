5b0afa0cd4ce564976e0179b14cb425c1b263fea
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-26 14:55:18 -0800

    move

--- src/streamlit/index.tsx
+++ src/streamlit/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import SomehowRequired from "./SomehowRequired"
-import Leaderboard from "./Leaderboard"
+import Leaderboard from "../Leaderboard"
 
 ReactDOM.render(
   <React.StrictMode>
