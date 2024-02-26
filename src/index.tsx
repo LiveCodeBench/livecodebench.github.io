41afe72442155573a336af0fc09ca5aca69647b1
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-26 14:56:18 -0800

    test

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Leaderboard from "./Leaderboard"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>Hi</p>
+    <Leaderboard />
+    <p>Hello</p>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
