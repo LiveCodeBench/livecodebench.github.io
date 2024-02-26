99b46218c8dbd29380994e049cbb12d4f1d01270
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-26 15:09:07 -0800

    build again

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Leaderboard from "./Leaderboard"
 
+import mockData from "./mocks/2024_02_26.json"
+
 ReactDOM.render(
   <React.StrictMode>
     <p>Hi</p>
-    <Leaderboard />
+    <Leaderboard {...mockData} />
     <p>Hello</p>
   </React.StrictMode>,
   document.getElementById("root")
