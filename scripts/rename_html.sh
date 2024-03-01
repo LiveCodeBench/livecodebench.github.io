7331b79125e674bfd5189ed6545cc3b459276079
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-29 17:33:47 -0800

    it works

---
+++ scripts/rename_html.sh
@@ -0,0 +1,5 @@
+# Rename build/index.html to build/leaderboard.html
+# Rename build/index_home.thml to build/index.html
+
+mv build/index.html build/leaderboard.html
+mv build/index_home.html build/index.html
