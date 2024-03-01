4a148504b945cfe0f9150936a5234e72b01ca02a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-29 17:35:16 -0800

    does it work

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import mockData from "./mocks/2024_02_26.json"
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>LiveCodeBench</h1>
+    <h1 style={{ textAlign: "center" }}>LiveCodeBench</h1>
     <Leaderboard theme={{ base: "light" }} args={mockData} />
   </React.StrictMode>,
   document.getElementById("root")
