3c31ef1861f73fd7d2c38c79d23a04b952208fad
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-29 18:13:50 -0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -6,27 +6,35 @@ import mockData from "./mocks/2024_02_26.json"
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <h1 className="title is-1 publication-title">
-        LiveCodeBench: Holistic and Contamination Free Evaluation of Large
-        Language Models for Code
-      </h1>
-      <div className="column has-text-centered">
-        <div className="publication-links">
-          <span className="link-block">
-            <a
-              href="/"
-              className="external-link button is-normal is-rounded is-dark"
-            >
-              <span className="icon">
-                <i className="fas fa-home"></i>
-              </span>
-              <span>Home</span>
-            </a>
-          </span>
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container is-max-desktop">
+          <div className="columns is-centered">
+            <div className="column has-text-centered">
+              <h1 className="title is-1 publication-title">
+                LiveCodeBench: Holistic and Contamination Free Evaluation of
+                Large Language Models for Code
+              </h1>
+              <div className="column has-text-centered">
+                <div className="publication-links">
+                  <span className="link-block">
+                    <a
+                      href="/"
+                      className="external-link button is-normal is-rounded is-dark"
+                    >
+                      <span className="icon">
+                        <i className="fas fa-home"></i>
+                      </span>
+                      <span>Home</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
     <Leaderboard theme={{ base: "light" }} args={mockData} />
   </React.StrictMode>,
   document.getElementById("root")
