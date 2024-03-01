8f9ed9b5928cdd887d3c66391129119eb583515e
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2024-02-29 17:59:13 -0800

    try

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,20 @@ import mockData from "./mocks/2024_02_26.json"
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 style={{ textAlign: "center" }}>LiveCodeBench</h1>
+    <div>
+      <h1 style={{ textAlign: "center" }}>LiveCodeBench</h1>
+      <span className="link-block">
+        <a
+          href="index.html"
+          className="external-link button is-normal is-rounded is-dark"
+        >
+          <span className="icon">
+            <i className="fas fa-home"></i>
+          </span>
+          <span>Home</span>
+        </a>
+      </span>
+    </div>
     <Leaderboard theme={{ base: "light" }} args={mockData} />
   </React.StrictMode>,
   document.getElementById("root")
